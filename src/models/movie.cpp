a6c7211ffa63fef46a674a9a0aba50f63c9cdc88
Marlon Vega <mvegach2013@gmail.com>
2019-09-23 14:00:57 -0600

    Class to container movie data

---
+++ src/models/movie.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Movies{
+    public:
+        std::string title;
+        std::string release_date;
+        std::string genre;
+        std::string duration;
+        std::string synopsis;
+        std::string score;
+        //variable to hold poster image data
+};
